75a6016b14bb6fff9bc3df8f2b9d717f9ec67a36
Elitex <jain.yashu2211@gmail.com>
2024-01-03 01:24:39 +0530

    ok

--- Lab Programs/program22.c
+++ Lab Programs/program22.c
@@ -3,7 +3,7 @@
 
 void main(){
     int n, s, r, t, i;
-    
+    int t2;
     for(n=1;n<=100;n++){
         s = 0;
         t = n;
@@ -13,10 +13,13 @@ void main(){
             t /= 10;
         }
         t = n;
+        t2 = i;
         for(;t != 0;){
             r = t % 10;
-            while(i < 0){
+            i = t2;
+            while(i > 1){
                 r *= r;
+                i--;
             }
             s = s + r;
             t /= 10;
