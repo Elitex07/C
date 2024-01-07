299fc8d4dec038dbb09eb764fbc159cf659287c7
Elitex <jain.yashu2211@gmail.com>
2024-01-07 10:04:55 +0530

    Outputs

--- Lab Programs/program28.c
+++ Lab Programs/program28.c
@@ -51,7 +51,7 @@ int main(){
         
         for(i=0;i<m;i++){
             for(j=0;j<n;j++){
-                mat3[i][j] = mat1[m][n] + mat2[m][n];
+                mat3[i][j] = mat1[i][j] + mat2[i][j];
             }
         }
 
